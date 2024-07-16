2087e170666739c150ac863aaac77d1554da7aca
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-07-16 17:59:27 +0500

    button style has been change

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 import styles from "./Button.module.scss";
 
 const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return <button className={styles.buttons} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
